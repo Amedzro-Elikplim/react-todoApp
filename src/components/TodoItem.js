4852208179d9e992f464e12077803a39a3216ff2
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-30 21:09:16 +0000

    Revert "Add checkbox to todo item"
    This reverts commit fe1e27a77554991f0c9acfdf7532a90dd146dc3a.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TodoItem = (props) => {
         return (
-            <li> <input type="checkbox" /> {props.todo.title}</li>
+            <li>{props.todo.title}</li>
         )
 }
 
