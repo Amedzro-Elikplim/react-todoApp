3e12d4ad9fa6ec1c84226718f02ec1b453e79c41
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-30 17:35:07 +0000

    initial commit

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+
+class TodoContainer extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello world</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default TodoContainer
