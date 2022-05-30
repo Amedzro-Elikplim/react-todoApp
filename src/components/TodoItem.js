fe1e27a77554991f0c9acfdf7532a90dd146dc3a
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-30 18:26:22 +0000

    Add checkbox to todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TodoItem = (props) => {
         return (
-            <li>{props.todo.title}</li>
+            <li> <input type="checkbox" /> {props.todo.title}</li>
         )
 }
 
