4ec9510c70c70a3f2df33a11e7e36afec6fa3305
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-30 21:07:13 +0000

    Fix merge conflict

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-const TodoItem = (props) => (
-  <li>
-    <input type="checkbox" checked={props.todo.completed} />
-    {props.todo.title}
-  </li>
-);
+const TodoItem = (props) => {
+        return (
+            <li> <input type="checkbox" /> {props.todo.title}</li>
+        )
+}
 
 export default TodoItem;
